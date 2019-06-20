2fa86a46514615693114e2415ecfe37a720d2134
Quikcall <39344604+Quikcall@users.noreply.github.com>
2019-06-20 04:55:21 +0100

    pip install + mysql database source file

---
+++ install_dependencies.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir MOSCA_app
+cd MOSCA_app
+python3 -m venv venv
+. venv/bin/activate
+pip install Flask
+apt-get install default-libmysqlclient-dev
+pip install flask_mysqldb
+pip install wtforms
+pip install passlib
+mysql -u root -p -e 'source mosca_tables.sql'
