80d5b7ff6bdccd0ca40eedad6a55703c41d4e3bb
Quikcall <39344604+Quikcall@users.noreply.github.com>
2019-06-21 14:50:34 +0100

    Update mosca_tables.sql

--- mosca_tables.sql
+++ mosca_tables.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE `try` DEFAULT CHARACTER SET latin1;
+CREATE DATABASE `myproject` DEFAULT CHARACTER SET latin1;
 
-USE `try`;
+USE `myproject`;
 
 DROP TABLE IF EXISTS `projects`;
 DROP TABLE IF EXISTS `samples`;
@@ -86,4 +86,3 @@ CREATE TABLE `users` (
  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
-INSERT INTO `users`(`name`,`username`,`email`,`password`) VALUES (`teste teste`, `teste`, `teste@teste.teste`, `teste`);
